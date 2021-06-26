680d8a87129309d85f664e656af63c46f994363d
parthpatel6347 <73423177+parthpatel6347@users.noreply.github.com>
2021-06-26 18:57:43 +0530

    first commit

---
+++ routes/contacts.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("get contacts");
+});
+
+router.post("/", (req, res) => {
+  res.send("add contact");
+});
+
+router.put("/:id", (req, res) => {
+  res.send("edit contact");
+});
+
+router.delete("/:id", (req, res) => {
+  res.send("delete contact");
+});
+
+module.exports = router;
