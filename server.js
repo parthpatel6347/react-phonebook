d5c0c36238b812738b8216175e3ce542adb7601a
parthpatel6347 <73423177+parthpatel6347@users.noreply.github.com>
2021-06-26 19:25:17 +0530

    setup mongo connection

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+//Connect to mongoDB
+connectDB();
 
 app.get("/", (req, res) => {
   res.json({ message: "hello from Phonebook API" });
 });
 
+//Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
