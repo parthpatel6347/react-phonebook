3a2d6a6c3e540c204c668c2be14972dda4d5e2f1
parthpatel6347 <73423177+parthpatel6347@users.noreply.github.com>
2021-07-03 12:27:48 +0530

    start login styles

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import AlertContext from "./context/alert/alertContext";
 import AuthContext from "./context/auth/authContext";
+import "./styles/Login.css";
 
 function Login(props) {
   const [user, setUser] = useState({
@@ -37,29 +38,34 @@ function Login(props) {
   };
 
   return (
-    <div>
-      <h1> Login</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={handleChange}
-          />
+    <div className="root">
+      <div className="container">
+        <h1>phoneBook</h1>
+        <div className="form-container">
+          <h2>Sign in</h2>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+              />
+            </div>
+            <input type="submit" value="Login" />
+          </form>
         </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={handleChange}
-          />
-        </div>
-        <input type="submit" value="Login" />
-      </form>
+      </div>
     </div>
   );
 }
