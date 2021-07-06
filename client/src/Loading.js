adaa26190cb9321f613e304def2d5b3df6d28862
parthpatel6347 <73423177+parthpatel6347@users.noreply.github.com>
2021-07-06 21:51:04 +0530

    add spinner

--- client/src/Loading.js
+++ client/src/Loading.js
@@ -1,11 +1,23 @@
 import React, { Fragment } from "react";
+import { Spinner } from "react-bootstrap";
 
 function Loading() {
   return (
     <Fragment>
       <div
-        style={{ width: "200px", height: "200px", backgroundColor: "blue" }}
-      ></div>
+        style={{
+          display: "flex",
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "400px",
+        }}
+      >
+        <Spinner
+          style={{ color: "#8F00B9", width: "50px", height: "50px" }}
+          animation="border"
+        />
+      </div>
     </Fragment>
   );
 }
